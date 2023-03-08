2d73bb9e06d41ef90bdae4d5494ca10a3d5afea7
AAKAAASSHHH24 <akashrksht@gmail.com>
2023-03-09 03:15:02 +0530

    football expert app created

---
+++ football_history.py
@@ -0,0 +1,36 @@
+import gradio as gr
+import openai, subprocess
+import json
+
+with open("key.json") as file:
+  data  = json.load(file)
+  openai.api_key = data['key']
+
+
+messages = [{"role": "system", 
+             "content": 'You are an expert about football history who has a deep knowledge about the games played in the past.You can even speak up stats from the past expertly.'}]
+
+def transcribe(audio):
+    global messages
+
+    audio_file = open(audio, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+
+    messages.append({"role": "user", "content": transcript["text"]})
+
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+
+    system_message = response["choices"][0]["message"]
+    messages.append(system_message)
+
+    #subprocess.call(["say", system_message['content']])
+
+    chat_transcript = ""
+    for message in messages:
+        if message['role'] != 'system':
+            chat_transcript += message['role'] + ": " + message['content'] + "\n\n"
+
+    return chat_transcript
+
+ui = gr.Interface(fn=transcribe, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text").launch()
+ui.launch(share=True)
